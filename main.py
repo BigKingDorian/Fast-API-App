979539f2c3a4fd5951ac7fbbf9135b3b1512c580
BigKingDorian <futurelinkwebdesign@gmail.com>
2025-09-10 17:21:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -452,7 +452,6 @@ async def media_stream(ws: WebSocket):
             deepgram_options = {
                 "punctuate": True,
                 "interim_results": True,
-                "endpointing": 2000  # 🟢 Wait 2000ms of silence before finalizing
                 }
             
             dg_connection = await asyncio.to_thread(live_client.v, "1")
