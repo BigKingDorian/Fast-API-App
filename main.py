4116107eb42febec0e9a0704514c11896b8bb4b4
BigKingDorian <futurelinkwebdesign@gmail.com>
2025-09-11 18:48:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 load_dotenv("/root/Fast-API-App/.env")
 
 # 🗂️ Log file config
-LOG_DIR = "/root/logs"
+LOG_DIR = "logs"
 LOG_FILE = f"{LOG_DIR}/app.log"
 os.makedirs(LOG_DIR, exist_ok=True)
 
